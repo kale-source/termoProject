b5c00461de3001cca807a8ee643d5bbc3858a6b4
Kaiky <kaikychimbum11@gmail.com>
2025-04-21 21:07:58 -0300

    Correção de bugs | Termo PYTHON | 21/04/2025

--- logic.py
+++ logic.py
@@ -1,40 +1,53 @@
 def gameLogic(randomWord, wordTyped):
     win = 0
+    letters_check = []
+    
     # Faz a contagem da letra existente no randomWord
-    def quantityLetters(typedLetter):
+    def checkLetters(typedLetter):
         countTotal = 0
-        for count in wordTyped:
-            if count == typedLetter: countTotal += 1
-        return countTotal
+        for countRandom in randomWord:
+            if countRandom == typedLetter: countTotal += 1
+        if typedLetter not in letters_check:
+            letters_check.append(typedLetter)
+            return True
+        else:
+            check_quantity = 0
+            for check in letters_check:
+                if check == typedLetter: check_quantity += 1
+            if check_quantity < countTotal:
+                letters_check.append(typedLetter)
+                return True
+            else:
+                return False
 
     # Função para comparar a palavra digitada pelo usuário com a palavra sorteada e a posição
     def checkPosition(indexTyped, letterTyped):
-        for index, random in enumerate(randomWord):
-            if letterTyped == random and index == indexTyped:
-                return True
-        return False
+        for indexRandom, randomTyped in enumerate(randomWord):
+            if letterTyped == randomTyped and indexRandom == indexTyped:
+                return True # Tem a letra e está na posição correta e retorna true
+        return False # Retorna False ou seja, tem na palavra, mas está na posição errada
 
     # For para palavra digitada pelo usuário
     for index, typed in enumerate(wordTyped):
         if typed in randomWord:
-            quantityLetters(typed)
+            checkResult = checkLetters(typed)
             result = checkPosition(index, typed)
-            if result == False:
-                quantity = quantityLetters(typed)
-                if quantity != 1:
-                    print(f'{'\033[90m'}{typed}{'\033[0m'}', end=' ')
+            if checkResult == True:
+                if result == False:
+                    print(f'{'\033[93m'}{typed}{'\033[0m'}', end=' ') # Retorna amarelo
                 else:
-                    print(f'{'\033[93m'}{typed}{'\033[0m'}', end=' ')
+                    win += 1
+                    print(f'{"\033[32m"}{typed}{'\033[0m'}', end=' ') # Retorna verde
             else:
-                win += 1
-                print(f'{"\033[32m"}{typed}{'\033[0m'}', end=' ')
+                print(f'{'\033[90m'}{typed}{'\033[0m'}', end=' ')
         else:
-            print(f'{'\033[90m'}{typed}{'\033[0m'}', end=' ')
+            print(f'{'\033[90m'}{typed}{'\033[0m'}', end=' ') # Retorna cinza
 
     if win == 5:
         return 'Win'
     else:
         return 'Lose'
+    
     # False = Amarelo
     # True = Verde
     # None = Cinza
