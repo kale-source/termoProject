1e5e593df0c03ac08bd9047738f038cec804b707
Kaiky <kaikychimbum11@gmail.com>
2025-04-26 00:06:31 -0300

    Otimização e adição das cores ANSIS | 26/04/2025

--- logic.py
+++ logic.py
@@ -1,5 +1,7 @@
 def gameLogic(randomWord, wordTyped):
     dictLetter = {'0': '', '1': '', '2': '', '3': '', '4': ''}
+    index_finish = 0
+    win = 0
 
     def addDict(index, color):
         if index == 0:
@@ -17,14 +19,13 @@ def gameLogic(randomWord, wordTyped):
         letters_quantity = 0
         
         for letters in randomWord:
-            if letters == letterTyped: letters_quantity += 1
-        
+            if letters == letterTyped: letters_quantity += 1 
         return letters_quantity
     
     letters_check = list()
     indexList = []
 
-    # Identifica os verdes
+    # Função para identifica os verdes antes dos amarelos
     def green_check(index, letter):
         for indexRandom, letterRandom in enumerate(randomWord):
             if indexRandom == index and letterRandom == letter:
@@ -32,25 +33,21 @@ def gameLogic(randomWord, wordTyped):
                 indexList.append(index)
                 letters_check.append(letter)
                 return True
-            
-        return False
     
-    # Identifica os amarelos depois dos verdes
+    # Função chamada para identificar os amarelos depois dos verdes
     def yellow_check(index, letter):
         quantity_total = lettersCounter(letter)
         quantity_check = 0
         indexStr = ''
 
         for i in indexList: indexStr += str(i)
-
         for check in letters_check:
             if check == letter: quantity_check += 1
 
         if quantity_total - quantity_check != 0 and str(index) not in indexStr:
             addDict(index, 'yellow')
             letters_check.append(letter)
-        elif str(index) not in indexStr:
-            addDict(index, 'grey')
+        elif str(index) not in indexStr: addDict(index, 'grey')
 
     def checkLetters():
         # Verificar verdes e letras únicas
@@ -59,9 +56,7 @@ def gameLogic(randomWord, wordTyped):
                 quantity_letters = lettersCounter(letterTyped)
                 if quantity_letters == 1:
                     if letterTyped not in letters_check:
-                        result = green_check(indexTyped, letterTyped)
-                        if result == False:
-                            addDict(indexTyped, 'grey')
+                        green_check(indexTyped, letterTyped)
                     else:
                         addDict(indexTyped, 'grey')
                 else:
@@ -75,9 +70,19 @@ def gameLogic(randomWord, wordTyped):
                 yellow_check(indexTyped, letterTyped)
                 
     checkLetters()
-    print(dictLetter)
+    
+    # Input das letras com as cores
+    for dictValue in dictLetter.values():
+        if dictValue == 'green':
+            print(f'\033[32m{wordTyped[index_finish]}\033[0m', end=' ')
+            index_finish += 1
+        elif dictValue == 'yellow':
+            print(f'\033[33m{wordTyped[index_finish]}\033[0m', end=' ')
+            index_finish += 1
+        elif dictValue == 'grey':
+            print(f'\033[90m{wordTyped[index_finish]}\033[0m', end=' ')
+            index_finish += 1
 
-    win = 0
     for value_dict in dictLetter.values():
         if value_dict == 'green': win += 1
 
